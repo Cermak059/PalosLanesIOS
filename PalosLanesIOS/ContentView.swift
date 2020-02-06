a39855a8176ef1e1fd908387ecbdcd2872d40b73
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2020-02-06 14:56:00 -0600

    Initial Commit

---
+++ PalosLanesIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PalosLanesIOS
+//
+//  Created by Kyle Cermak on 2/6/20.
+//  Copyright © 2020 Kyle Cermak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
